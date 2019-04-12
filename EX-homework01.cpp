a32c98db97b44b8cd2bcfcf1ed50cd057dcbe7d6
zoxinc10061 <48340647+zoxinc10061@users.noreply.github.com>
2019-04-12 08:56:30 +0800

    Add files via upload

---
+++ EX-homework01.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
+{
+	char i;
+	for (i=65; i<=90; i++)
+	{
+		printf("%c ",i);
+	}
+	
+	printf("\n");
+	for (i=97; i<=122; i++)
+	{
+		printf("%c ",i);
+	}
+	printf("\n");
+	system ("pause");
+	return 0;
+}
